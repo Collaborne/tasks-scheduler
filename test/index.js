79eedd2d1261d19cbd298a6b721a5b340e998d5a
mlazzarini <margherita.lazzarini@gmail.com>
2018-03-02 16:23:37 +0100

    Simplify test data

--- test/index.js
+++ test/index.js
@@ -3,22 +3,23 @@ const expect = chai.expect;
 
 const {schedule} = require('../src/index');
 
-const FEB_23_DATE = '2018-02-23';
-const APR_20_DATE = '2018-04-20';
-const MAR_23_DATE = '2018-03-23';
+// Monday 5 March, 2018
+const START_DATE = '2018-03-05';
+
+// Thursday 8 March, 2018
+const END_DATE_8_MARCH = '2018-03-08';
+
+// Tuesday 13 March, 2018
+const END_DATE_13_MARCH = '2018-03-13';
 
 const TASKS = [
 	{
-		id: 'create-ux-design',
-		nrNormDays: 6,
+		id: 'First-task',
+		nrNormDays: 1.4
 	},
 	{
-		id: 'implement-scheduling-lib',
-		nrNormDays: 10,
-	},
-	{
-		id: 'implement-ui',
-		nrNormDays: 4,
+		id: 'Second-task',
+		nrNormDays: 1.4
 	},
 ];
 
@@ -26,65 +27,65 @@ describe('Task scheduler', () => {
 	describe('correctly calculates end date', () => {
 		it('with 100% time allocation', () => {
 			const inputParams = {
-				start: FEB_23_DATE,
+				start: START_DATE,
 				tasks: TASKS,
 				timeAllocation: 1,
 			};
 			const result = schedule(inputParams);
-			expect(result.end).to.be.equals(MAR_23_DATE);
+			expect(result.end).to.be.equals(END_DATE_8_MARCH);
 		});
 
 		it('with 50% time allocation', () => {
 			const inputParams = {
-				start: FEB_23_DATE,
+				start: START_DATE,
 				tasks: TASKS,
 				timeAllocation: 0.5,
 			};
 			const result = schedule(inputParams);
-			expect(result.end).to.be.equals(APR_20_DATE);
+			expect(result.end).to.be.equals(END_DATE_13_MARCH);
 		});
 	});
 
 	describe('correctly calculates time allocation', () => {
-		it('of 100% from 23 Feb. to 23 Mar. for a 20 days project', () => {
+		it('of 100% from 12 Feb. to 23 Mar. for a 20 days project', () => {
 			const inputParams = {
-				end: MAR_23_DATE,
-				start: FEB_23_DATE,
+				end: END_DATE_8_MARCH,
+				start: START_DATE,
 				tasks: TASKS,
 			};
 			const result = schedule(inputParams);
-			expect(result.timeAllocation).to.be.equals(1);
+			expect(result.timeAllocation).to.be.closeTo(1, 0.1);
 		});
 
-		it('of 50% from 23 Feb. to 4 Apr. for a 20 days project', () => {
+		it('of 50% from 12 Feb. to 20 Apr. for a 20 days project', () => {
 			const inputParams = {
-				end: APR_20_DATE,
-				start: FEB_23_DATE,
+				end: END_DATE_13_MARCH,
+				start: START_DATE,
 				tasks: TASKS,
 			};
 			const result = schedule(inputParams);
-			expect(result.timeAllocation).to.be.equals(0.5);
+			expect(result.timeAllocation).to.be.closeTo(0.5, 0.1);
 		});
 	});
 
 	describe('correctly calculates total project days', () => {
 		it('for a set of given tasks', () => {
 			const inputParams = {
-				end: MAR_23_DATE,
-				start: FEB_23_DATE,
+				end: END_DATE_8_MARCH,
+				start: START_DATE,
 				tasks: TASKS,
 			};
 			const result = schedule(inputParams);
-			expect(result.nrNormDays).to.be.equals(20);
+			expect(result.nrNormDays).to.be.equals(2.8);
 		});
 	});
 
 	describe('correctly calculates deadlines', () => {
 		it('for a set of given tasks', () => {
-			const EXPECTED_DEADLINES = ['2018-03-05', '2018-03-19', MAR_23_DATE];
+			const EXPECTED_DEADLINES = ['2018-03-07', END_DATE_8_MARCH];
 			const inputParams = {
-				end: MAR_23_DATE,
-				start: FEB_23_DATE,
+				end: END_DATE_8_MARCH,
+				start: START_DATE,
 				tasks: TASKS,
 			};
 			const result = schedule(inputParams);
@@ -92,27 +93,33 @@ describe('Task scheduler', () => {
 			for (let i = 0; i < result.deadlines.length; i++) {
 				expect(result.deadlines[i].deadline).to.be.equals(EXPECTED_DEADLINES[i]);
 			}
+			// Check also that last task's deadline equals the end of the project
+			const lastDeadline = result.deadlines[TASKS.length - 1].deadline;
+			expect(result.end).to.be.equals(lastDeadline);
 		});
 
 		it('with blocked periods', () => {
 			const BLOCKED_PERIODS = [
 				{
-					end: '2018-02-26',
-					start: '2018-02-28',
+					end: '2018-03-09',
+					start: '2018-03-07',
 				}
 			];
-			const EXPECTED_DEADLINES_WITH_BLOCKS = ['2018-03-07', '2018-03-23', '2018-04-02'];
+			const EXPECTED_DEADLINES_WITH_BLOCKS = ['2018-03-12', END_DATE_13_MARCH];
 			const inputParams = {
 				blockedPeriods: BLOCKED_PERIODS,
-				end: MAR_23_DATE,
-				start: FEB_23_DATE,
+				start: START_DATE,
 				tasks: TASKS,
+				timeAllocation: 1,
 			};
 			const result = schedule(inputParams);
 			expect(result.deadlines).to.have.lengthOf(inputParams.tasks.length);
 			for (let i = 0; i < result.deadlines.length; i++) {
 				expect(result.deadlines[i].deadline).to.be.equals(EXPECTED_DEADLINES_WITH_BLOCKS[i]);
 			}
+			// Check also that last task's deadline equals the end of the project
+			const lastDeadline = result.deadlines[TASKS.length - 1].deadline;
+			expect(result.end).to.be.equals(lastDeadline);
 		});
 	});
 });
